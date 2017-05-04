de206584c7f3dc4a4e122e8ff367caa3b582fb47
axl <axel.catusse@gmail.com>
2017-05-04 16:43:29 +0800

    importing files

---
+++ lib/ngclipboard.rb
@@ -0,0 +1,6 @@
+require "ngclipboard/version"
+
+module Ngclipboard
+  class Engine < ::Rails::Engine
+  end
+end
---
+++ vendor/assets/javascripts/ngclipboard.js
@@ -0,0 +1,3 @@
+// ngclipboard dependencies as a manifest
+//= require clipboard.min
+//= require ngclipboard.min
